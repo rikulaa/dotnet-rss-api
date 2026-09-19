7e7dc1f30e8f5af09dadb3c14d6d2ae370788f1d
Riku Laajala <riku.laajala@gmail.com>
2026-09-19 20:44:51 +0300

    Move db to local folder

--- AppContext.cs
+++ AppContext.cs
@@ -9,9 +9,7 @@ public class AppContext : DbContext
 
     public AppContext()
     {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "app.db");
+        DbPath = Path.Combine(Directory.GetCurrentDirectory(), "app.db");
     }
 
     // The following configures EF to create a Sqlite database file in the
