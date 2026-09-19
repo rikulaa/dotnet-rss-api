7306f58556bb4117ec79378c19eb4c24b5663066
Riku Laajala <riku.laajala@gmail.com>
2026-09-20 00:30:12 +0300

    Print sql queries to console

--- AppContext.cs
+++ AppContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Api.Domain.Feed;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 public class AppContext : DbContext
 {
@@ -16,7 +17,11 @@ public class AppContext : DbContext
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+    {
+        options.UseSqlite($"Data Source={DbPath}");
+        // Only sql commands executed
+        options.LogTo(Console.WriteLine, (eventId, logLevel) => eventId == RelationalEventId.CommandExecuted);
+    } 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
